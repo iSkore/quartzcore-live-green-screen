3ca4d785bff4ec35986f34b0f67c6f8ad8e5d6c3
Nick Soggin <nick@exploreplanet3.com>
2016-04-13 19:18:58 -0400

    Added releases for low-mid-high selectivity ranges.
    Added ascii reactive imaging.
    
    Added Jelly.qtz original - Snow Leopard team

--- overlayFilter.cpp
+++ overlayFilter.cpp
@@ -16,10 +16,10 @@ kernel vec4 darkToTransparent(sampler image)
 
 	// HIGHLY SELECTIVE
 	color.a = ( color.g <= 0.35 && color.g >= 0.2 && color.r <= 0.2 && color.b <= 0.275 && color.b >= 0.05 ) ? 0.0 : 1.0;
-
+	
 	// MID SELECTIVE
 	color.a = ( color.r <= 0.175 && color.g <= 0.35 && color.g >= 0.15 && color.b <= 0.25 && color.b >= 0.05 ) ? 0.0 : 1.0;
-
+	
 	// LESS SELECTIVE
 	color.a = ( color.r <= 0.155 && color.r >= 0.0 && color.g <= 0.35 && color.g >= 0.135 && color.b <= 0.2175 && color.b >= 0.0375 ) ? 0.0 : 1.0;
 
---
+++ spectrum.md
@@ -0,0 +1,3 @@
+[spectrum splitting][0]
+
+[0]:http://superfleamedia.blogspot.com/2011/05/tutorial-structures-and-index-members.html
